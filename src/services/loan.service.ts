f0165153d0cc39e6f71174a819fafd0132fd1511
Sten-Mark Laur <sten@sml.ee>
2024-10-24 11:12:12 +0300

    Use decimal

--- src/services/loan.service.ts
+++ src/services/loan.service.ts
@@ -1,10 +1,11 @@
 import type { LoanApplication } from '@/interfaces/loan-application.interface'
+import Decimal from 'decimal.js'
 
 export const processLoanApplication = async (data: LoanApplication) => {
   // This is a mock API call that returns a promise
   return new Promise(resolve => {
     setTimeout(() => {
-      if (data.applicantMonthlyIncome < 1000) {
+      if (new Decimal(data.applicantMonthlyIncome).lessThan(1000)) {
         resolve(false)
       } else {
         resolve(true)
